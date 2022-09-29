bd63d91f345d1356737ee04250a1ec61b51b5009
Brendan Rodrigues <brendanrodrigues33@gmail.com>
2022-09-29 19:05:17 -0400

    added plugin function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+var plugin =function (options) {
+    var seneca = this
+// add, get  and delete
+    seneca.add({ role:'product', cmd: 'add'}, function(msg, respond){
+        this.make('product').data$(msg.data).save$(respond)
+    })
+
+    seneca.add({role:'product', cmd: 'get-all'}, function (msg, respond){
+        this.make('product').list$({}, respond)
+    })
+
+    // delete all sequencially
+   /* seneca.add({role:'product', cmd:'delete-all'}, function(msg,respond){
+        this.make('product').remove$({},respond)
+    })*/
+}
 
